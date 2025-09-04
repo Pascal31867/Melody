e9dfb73105f55a588fd3524dea3d8d7170700e1a
Pique <68230014+Pascal31867@users.noreply.github.com>
2025-09-04 23:11:59 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hey Melody',
+    introDesc: `We haven't met for a long time and I miss you so much, We should hang out soon.`,
+    btnIntro: 'right <3',
+    title: 'Should we be do a getaway in Mombasa or movie night? 🤔',
+    desc: 'Movie night at your place, I will bring the popcorn and drinks, you choose the movie :3',
+    btnYes: 'Movie at my place',
+    btnNo: 'Mombasa getaway',
+    question:'I knew you want me at your place 😂😂, but',
+    btnReply: 'Submit',
+    reply: 'Nilijua hautajaza kitu 😂😂, ama wacha tu 😂',
+    mess: 'Nilijua hautajaza kitu 😂😂, ama wacha tu 😂',
+    messDesc: 'So site ikoje?.',
+    btnAccept: 'Message the GURU',
+    messLink: 'https://wa.me/254769565933' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
